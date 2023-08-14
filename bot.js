93f10f53e1b65a6b97df6b42e219621433a5cbd1
Connor MacGregor <macgregoco@gmail.com>
2023-08-13 21:50:02 -0500

    Upgrade to discord.js v14. Added dotenv support
    Also removed old startup process since it wasn't working with 14.

--- bot.js
+++ bot.js
@@ -1,35 +1,14 @@
-const Discord = require("discord.js");
-const Enmap = require("enmap");
-const fs = require("fs");
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+require('dotenv').config();
 
-const client = new Discord.Client();
-const config = require("./config.json");
-const auth = require("./auth.json");
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+// const config = require("./config.json");
 
 // We also need to make sure we're attaching the config to the CLIENT so it's accessible everywhere!
-client.config = config;
+// client.config = config;
 
-//read events handled from filesystem and map them
-fs.readdir("./events/", (err, files) => {
-    if (err) return console.error(err);
-    files.forEach((file) => {
-        const event = require(`./events/${file}`);
-        let eventName = file.split(".")[0];
-        client.on(eventName, event.bind(null, client));
-    });
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
-//read commands handled from filesystem and map them
-client.commands = new Enmap();
-fs.readdir("./commands/", (err, files) => {
-    if (err) return console.error(err);
-    files.forEach((file) => {
-        if (!file.endsWith(".js")) return;
-        let props = require(`./commands/${file}`);
-        let commandName = file.split(".")[0];
-        console.log(`Attempting to load command ${commandName}`);
-        client.commands.set(commandName, props);
-    });
-});
-
-client.login(auth.token);
+client.login(process.env.DISCORD_TOKEN);
