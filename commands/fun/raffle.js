0d4dd1497513acc1a1fd8ac19eb26175a09797b5
Connor MacGregor <macgregoco@gmail.com>
2023-09-17 14:49:48 -0500

    added guildvoicestates intent so raffle caller voice channel can be determined for each command run

--- commands/fun/raffle.js
+++ commands/fun/raffle.js
@@ -6,12 +6,11 @@ module.exports = {
         .setDescription('Pick a winner of the raffle from users in the same voice chat as the caller'),
         async execute(interaction) {
             let responseMessage = `You must be in a voice channel to raffle`;
-            const voiceChannel = interaction.guild.members.cache.get(interaction.member.user.id).voice.channel; 
+            const voiceChannel = interaction.member.voice.channel;
             
             if(voiceChannel)
             {
                 const voiceChannelMembers = voiceChannel.members;
-                console.log(voiceChannelMembers.size);
                 const winningMember = voiceChannelMembers.at(Math.floor(voiceChannelMembers.size * Math.random()));
                 
                 responseMessage = `${winningMember.user} won ${interaction.user}'s Raffle`;
