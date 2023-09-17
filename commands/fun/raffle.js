535fbc7dc44f88daa83320933e4fdc293620b9bf
Connor MacGregor <macgregoco@gmail.com>
2023-09-17 14:03:45 -0500

    raffle command now functions. @ing the winning user and the caller

--- commands/fun/raffle.js
+++ commands/fun/raffle.js
@@ -6,13 +6,15 @@ module.exports = {
         .setDescription('Pick a winner of the raffle from users in the same voice chat as the caller'),
         async execute(interaction) {
             let responseMessage = `You must be in a voice channel to raffle`;
-            const voiceChannelId = interaction.member.voice.Channel;
-            if(voiceChannelId)
+            const voiceChannel = interaction.guild.members.cache.get(interaction.member.user.id).voice.channel; 
+            
+            if(voiceChannel)
             {
-
-                const winningUser = voiceChannelId;
-                responseMessage = `${winningUser} won ${interaction.user.username}'s Raffle`;
-    
+                const voiceChannelMembers = voiceChannel.members;
+                console.log(voiceChannelMembers.size);
+                const winningMember = voiceChannelMembers.at(Math.floor(voiceChannelMembers.size * Math.random()));
+                
+                responseMessage = `${winningMember.user} won ${interaction.user}'s Raffle`;
             }
             await interaction.reply(responseMessage);
         }
