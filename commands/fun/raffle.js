5c82251bd6a0eaff154c5f9463d7cfe49a4a9599
Connor MacGregor <macgregoco@gmail.com>
2023-09-22 19:35:56 -0500

    added categories to commands

--- commands/fun/raffle.js
+++ commands/fun/raffle.js
@@ -5,17 +5,18 @@ module.exports = {
         .setName('raffle')
         .setDescription('Pick a winner of the raffle from users in the same voice chat as the caller')
         .setDMPermission(false),
-        async execute(interaction) {
-            let responseMessage = `You must be in a voice channel to raffle`;
-            const voiceChannel = interaction.member.voice.channel;
+    category: 'fun',
+    async execute(interaction) {
+        let responseMessage = `You must be in a voice channel to raffle`;
+        const voiceChannel = interaction.member.voice.channel;
+        
+        if(voiceChannel)
+        {
+            const voiceChannelMembers = voiceChannel.members;
+            const winningMember = voiceChannelMembers.at(Math.floor(voiceChannelMembers.size * Math.random()));
             
-            if(voiceChannel)
-            {
-                const voiceChannelMembers = voiceChannel.members;
-                const winningMember = voiceChannelMembers.at(Math.floor(voiceChannelMembers.size * Math.random()));
-                
-                responseMessage = `${winningMember.user} won ${interaction.user}'s Raffle`;
-            }
-            await interaction.reply(responseMessage);
+            responseMessage = `${winningMember.user} won ${interaction.user}'s Raffle`;
         }
+        await interaction.reply(responseMessage);
+    }
 }
--- commands/utility/info.js
+++ commands/utility/info.js
@@ -2,17 +2,18 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
-	.setName('info')
-	.setDescription('Get info about a user or a server!')
-	.addSubcommand(subcommand =>
-		subcommand
-			.setName('user')
-			.setDescription('Info about a user')
-			.addUserOption(option => option.setName('target').setDescription('The user')))
-	.addSubcommand(subcommand =>
-		subcommand
-			.setName('server')
-			.setDescription('Info about the server')),
+		.setName('info')
+		.setDescription('Get info about a user or a server!')
+		.addSubcommand(subcommand =>
+			subcommand
+				.setName('user')
+				.setDescription('Info about a user')
+				.addUserOption(option => option.setName('target').setDescription('The user')))
+		.addSubcommand(subcommand =>
+			subcommand
+				.setName('server')
+				.setDescription('Info about the server')),
+	category: 'utility',
     async execute(interaction) {
         if (interaction.options.getSubcommand() === 'user') {
 			const targetUser = interaction.options.getUser('target');
