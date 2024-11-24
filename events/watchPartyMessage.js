9fb7f65c19328f952feb0d0bb630e72b0d2a800f
Connor MacGregor <macgregoco@gmail.com>
2024-11-24 01:28:50 -0600

    preliminary url netflix title fetching

--- events/watchPartyMessage.js
+++ events/watchPartyMessage.js
@@ -1,10 +1,15 @@
-const { Events } = require('discord.js');
+const Events = require('discord.js');
+const cheerio = require('cheerio');
+const axios = require('axios');
 
 const telePartyPrefix = 'https://www.netflix.com/watch/';
+
 module.exports = {
 	name: Events.MessageCreate,
 	once: false,
 	async execute(message) {
+		console.log("Event: watchPartMessage");
+		console.log(`Message from ${message.author.tag}: ${message.content}`);
 		try {
 			if (message.author.bot) return;
 
@@ -13,7 +18,9 @@ module.exports = {
 				const videoId = message.content.split('/')[4].split('?')[0];
 
 				// netflix service reaches out to netflix and gets the show name from the results
+				const videoTitle = GetVideoTitle(videoId);
 
+				console.log(videoTitle);
 			}
 		}
 		catch (error) {
@@ -21,3 +28,37 @@ module.exports = {
 		}
 	},
 };
+
+GetVideoTitle = (videoId) => {
+	const netflixResponse = fetchNetflixData(videoId)
+	
+	return parseNetflixData(netflixResponse);
+}
+
+//get video from netflix
+fetchNetflixData = async (videoId) => 
+{
+	const netflixUrlPrefix = "https://www.netflix.com/watch/" 
+	const url = netflixUrlPrefix + videoId
+	try {
+		const response = await axios.get(url);
+		if (response.status !== 200) {
+		  throw new Error(`Response status: ${response.status}`);
+		}
+
+		const html = await response.data;
+
+		const $ = cheerio.load(html);
+		const title = $('title').text();
+
+		return title.split('|')[0].split('Watch')[1];
+	  } catch (error) {
+		console.error(error.message);
+	  }
+}
+
+//parse info from netflix to get title
+parseNetflixData = (netflixData) =>
+{
+	return netflixData;
+}
